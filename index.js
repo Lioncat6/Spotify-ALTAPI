154305ef67c4da338538eb58282d0459cb7c2a0b
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-09-26 14:39:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function refreshToken() {
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Replace with your allowed origin(s)
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
